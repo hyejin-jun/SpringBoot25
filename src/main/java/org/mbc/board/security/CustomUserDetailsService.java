7dee30140a5a1b1120d26f4764ba43027070879d
220-12\hyejin <h77200407@gmail.com>
2025-08-04 11:39:30 +0900

    2025.08.04 최종 수정

--- src/main/java/org/mbc/board/security/CustomUserDetailsService.java
+++ src/main/java/org/mbc/board/security/CustomUserDetailsService.java
@@ -6,9 +6,12 @@ import org.mbc.board.domain.Member;
 import org.mbc.board.repository.MemberRepository;
 import org.mbc.board.security.dto.MemberSecurityDTO;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
